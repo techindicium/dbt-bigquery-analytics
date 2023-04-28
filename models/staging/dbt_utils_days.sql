3383fa57fa36b08d7255fda9844fd6929d6da302
Gabriel Amante <gabriel.amante@indicium.tech>
2023-04-28 01:18:10 -0300

    Removed comments from dbt_utils_day

--- models/staging/dbt_utils_days.sql
+++ models/staging/dbt_utils_days.sql
@@ -1,10 +1,10 @@
 {% set my_query %}
-    select cast({{dbt_utils.current_timestamp()}} as date)    /*Result: 2023-04-25*/
+    select cast({{dbt_utils.current_timestamp()}} as date)
 {% endset %}
 
 {% if execute %}
-    {% set today = run_query(my_query).columns[0].values()[0] %}    /*Result: 2023-04-25*/
-    {% set tomorrow = dbt_utils.dateadd('day', 1, "'" ~ today ~ "'") %} /*cast(datetime_add(cast( 2023-04-24 as datetime),interval 1 day)*/
+    {% set today = run_query(my_query).columns[0].values()[0] %}
+    {% set tomorrow = dbt_utils.dateadd('day', 1, "'" ~ today ~ "'") %}
     {% set start_date = var('dbt_bigquery_analytics')['bigquery_analytics_start_date'] %}
     {% else %}
     {% set tomorrow = ' ' %}
